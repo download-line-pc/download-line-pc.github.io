767f31c3b2d7675b2518f50f9367eb7a1eae0ff7
download-line-pc <159541391+download-line-pc@users.noreply.github.com>
2024-02-09 22:50:53 +0700

    Delete dashboard directory

--- dashboard/Check.php
+++
@@ -1,6 +0,0 @@
-<?
-if(isset($_SESSION['user_id'])){
-	$user_id=$_SESSION['user_id'];}
-		else if(!isset($_SESSION['user_id'])){
-			echo"<script>alert('Please Login');window.location='$site[site_name]/dashboard/index.php';</script>";exit;}
-?>
--- dashboard/SignOut.php
+++
@@ -1,5 +0,0 @@
-<?
-session_start();
-unset($_SESSION[user_id]);
-echo"<script>window.location='index.php';</script>";
-?>

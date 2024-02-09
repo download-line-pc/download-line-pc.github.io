55ffbbde3a6147e6b332dac98310b5946ca46279
download-line-pc <159541391+download-line-pc@users.noreply.github.com>
2024-02-09 22:52:05 +0700

    Delete nav.php

--- nav.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
-          <a href="<?php echo$site['site_name']?>"><img src="<?php echo$site['site_name']?>/assets/img/<?php echo$site['site_logo']?>" class="logo_xs"></a>
-        </div>
-        <div class="col-xl-8 col-lg-8 col-md-8 col-sm-6 col-12 text-center">
-          <h1 class="text-dark py-2 fs-2"><?php echo$site['site_title']?></h1>
-        </div>
-    </div>

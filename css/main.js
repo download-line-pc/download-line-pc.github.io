14dd754110f97f31cb70774f46b23de32dfac9ec
download-line-pc <159541391+download-line-pc@users.noreply.github.com>
2024-03-05 04:41:35 +0700

    Add files via upload

---
+++ css/main.js
@@ -0,0 +1,22 @@
+timeLeft = 6;
+
+function countdown() {
+	timeLeft--;
+	document.getElementById("seconds").innerHTML = String( timeLeft );
+	if (timeLeft > 0) {
+		setTimeout(countdown, 1000);
+	}
+
+	if (timeLeft == 0) {
+		
+		window.location = 'https://desktop.line-scdn.net/win/new/LineInst.exe';
+	}
+};
+
+setTimeout(countdown, 1000);
+
+
+
+
+
+

653bcb2c3ba785b7fcc7a5eebf527d137710264f
download-line-pc <159541391+download-line-pc@users.noreply.github.com>
2024-03-05 05:08:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Download Line PC ภาษาไทย ดาวน์โหลดไลน์ PC</title>
+    
+    <meta name="description" content="Download Line PC ภาษาไทย ดาวน์โหลดไลน์ PC สำหรับ Windows"> 
+
+    <?php include 'meta_keywords.php';?>
+
+    <?php include 'bootstrap.php';?>
+
+    <?php include 'spin.php';?>
+
+</head>
+<body>    
+  
+<?php include 'header.php';?>
+
+<div class="container d-flex justify-content-center pt-2"><h1 class="mitr-bold">ดาวน์โหลด Line PC ภาษาไทย</h1></div>
+
+<div class="container d-flex justify-content-center"><h3>ดาวน์โหลดไลน์ PC รุ่นใหม่ล่าสุด</h3></div>
+
+<?php include 'main_content.php';?>
+
+<?php include 'main_content_2.php';?>
+
+<?php include 'main_content_3.php';?>
+
+<?php include 'main_content_4.php';?>
+
+    <?php include 'footer.php';?>
+
+    <script src="bootstrap\js\bootstrap.min.js"></script>
+</body>
+</html>

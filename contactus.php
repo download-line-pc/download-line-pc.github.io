37d5be9e5ecb284cd2972b9c4bc0e5b41b3e8378
download-line-pc <159541391+download-line-pc@users.noreply.github.com>
2024-03-05 04:43:52 +0700

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>ติดต่อเรา</title>
+    
+    <meta name="description" content="ติดต่อเรา"> 
+
+    <?php include 'meta_keywords.php';?>
+
+    <?php include 'bootstrap.php';?>
+
+    <?php include 'spin.php';?>
+
+</head>
+<body>    
+  
+<?php include 'header_contactus.php';?>
+
+<div class="container ">
+
+<h1 class="mitr-bold mt-3">ติดต่อเรา</h1>
+
+<h4>    
+
+<p>Call center 02-023-9999</p>
+    
+<p>จันทร์-ศุกร์ (ยกเว้นวันหยุดนักขัตฤกษ์)</p>
+
+<p>เวลา 9.00-18.00 น.</p>
+
+
+
+
+
+
+
+</div>
+
+
+<?php include 'footer.php';?>
+
+<script src="bootstrap\js\bootstrap.min.js"></script>
+</body>
+</html>
---
+++ download.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>กำลังดาวน์โหลดไลน์ Line PC ภาษาไทย</title>
+    
+    <meta name="description" content="กำลังดาวน์โหลดไลน์ Line PC ภาษาไทย"> 
+
+    <?php include 'meta_keywords.php';?>
+
+    <?php include 'bootstrap.php';?>
+
+    <?php include 'spin.php';?>
+
+</head>
+<body>    
+  
+<?php include 'header_download.php';?>
+
+<?php include 'footer.php';?>
+
+<script src="bootstrap\js\bootstrap.min.js"></script>
+<script src="css\main.js"></script>
+</body>
+</html>
